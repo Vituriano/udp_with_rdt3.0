a6bfc1ac6284e1b142c0bee80b1ecd74a55e8c26
Vituriano <viturianooliveira@gmail.com>
2022-04-22 22:27:52 -0300

    segunda entrega

---
+++ UDPClient.py
@@ -0,0 +1,88 @@
+import socket
+import zlib
+import struct
+import binascii
+
+def checksum_calculator(data):
+
+    data = bin(int(binascii.hexlify(data),16))
+   
+    c1 = data[0:16] or '0'
+    c2 = data[16:32] or '0'
+    c3 = data[32:48] or '0'
+    c4 = data[48:64] or '0'
+ 
+    Sum = bin(int(c1, 2)+int(c2, 2)+int(c3, 2)+int(c4, 2))[2:]
+
+    if(len(Sum) > 16):
+        x = len(Sum)-16
+        Sum = bin(int(Sum[0:x], 2)+int(Sum[x:], 2))[2:]
+    if(len(Sum) < 16):
+        Sum = '0'*(16-len(Sum))+Sum
+ 
+    Checksum = ''
+    for i in Sum:
+        if(i == '1'):
+            Checksum += '0'
+        else:
+            Checksum += '1'
+    return Checksum
+
+
+def check_checksum_calculator(data, checksum):
+   
+    data = bin(int(binascii.hexlify(data),16))
+    checksum = bin(int(binascii.hexlify(bytes(checksum)),16))
+
+    c1 = data[0:16] or '0'
+    c2 = data[16:32] or '0'
+    c3 = data[32:48] or '0'
+    c4 = data[48:64] or '0'
+ 
+    ReceiverSum = bin(int(c1, 2)+int(c2, 2)+int(checksum, 2) +
+                      int(c3, 2)+int(c4, 2)+int(checksum, 2))[2:]
+ 
+    if(len(ReceiverSum) > 16):
+        x = len(ReceiverSum)-16
+        ReceiverSum = bin(int(ReceiverSum[0:x], 2)+int(ReceiverSum[x:], 2))[2:]
+ 
+    return int(ReceiverSum, 2) == 0
+
+source_port = 1112
+destination_port = 1111
+receiver_addr = ('127.0.0.1', destination_port)
+this_addr = ('127.0.0.1', source_port)
+
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_sock.bind(this_addr)
+
+expecting_seq = 0
+
+while True:
+    full_packet, sender_address = recv_sock.recvfrom(1024)
+
+    udp_header = full_packet[:16]
+    data = full_packet[16:]
+    udp_header = struct.unpack("!IIII", udp_header)
+    correct_checksum = udp_header[3]
+
+    print(data)
+    seq = int(data.decode()[0])
+    content = data[1:]
+
+    is_data_corrupted = check_checksum_calculator(data, correct_checksum)
+
+    if not is_data_corrupted:
+        checksum = checksum_calculator(str(seq).encode())
+        header = struct.pack("!II", int(checksum, 2), seq)
+        send_socket.sendto(header, receiver_addr)
+        if str(seq) == str(expecting_seq):
+            print(content.decode())
+            expecting_seq = 1 - expecting_seq
+    else:
+        negative_seq = 1 - expecting_seq
+        checksum = checksum_calculator(str(negative_seq).encode())
+        header = struct.pack("!II", int(checksum, 2), negative_seq)
+        send_socket.sendto(header, receiver_addr)
---
+++ UDPServer.py
@@ -0,0 +1,107 @@
+from audioop import add
+import socket
+import struct
+import binascii
+
+SEGMENT_SIZE = 16
+
+def checksum_calculator(data):
+
+    data = bin(int(binascii.hexlify(data),16))
+   
+    c1 = data[0:16] or '0'
+    c2 = data[16:32] or '0'
+    c3 = data[32:48] or '0'
+    c4 = data[48:64] or '0'
+ 
+    Sum = bin(int(c1, 2)+int(c2, 2)+int(c3, 2)+int(c4, 2))[2:]
+
+    if(len(Sum) > 16):
+        x = len(Sum)-16
+        Sum = bin(int(Sum[0:x], 2)+int(Sum[x:], 2))[2:]
+    if(len(Sum) < 16):
+        Sum = '0'*(16-len(Sum))+Sum
+ 
+    Checksum = ''
+    for i in Sum:
+        if(i == '1'):
+            Checksum += '0'
+        else:
+            Checksum += '1'
+    return Checksum
+
+
+def check_checksum_calculator(data, checksum):
+   
+    data = bin(int(binascii.hexlify(data),16))
+    checksum = bin(int(binascii.hexlify(bytes(checksum)),16))
+
+    c1 = data[0:16] or '0'
+    c2 = data[16:32] or '0'
+    c3 = data[32:48] or '0'
+    c4 = data[48:64] or '0'
+ 
+    ReceiverSum = bin(int(c1, 2)+int(c2, 2)+int(checksum, 2) +
+                      int(c3, 2)+int(c4, 2)+int(checksum, 2))[2:]
+ 
+    if(len(ReceiverSum) > 16):
+        x = len(ReceiverSum)-16
+        ReceiverSum = bin(int(ReceiverSum[0:x], 2)+int(ReceiverSum[x:], 2))[2:]
+ 
+    return int(ReceiverSum, 2) == 0
+
+with open("teste.txt") as f:
+    content = f.read()
+
+
+source_port = 1111
+destination_port = 1112
+receiver_addr = ('127.0.0.1', destination_port)
+this_addr = ('127.0.0.1', source_port)
+
+socket_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_sock.bind(this_addr)
+recv_sock.settimeout(1)
+
+offset = 0
+seq = 0
+
+while offset < len(content):
+    if offset + SEGMENT_SIZE > len(content):
+        segment = content[offset:]
+    else:
+        segment = content[offset:offset + SEGMENT_SIZE]
+
+    offset += SEGMENT_SIZE - seq
+
+    ack_received = False
+
+    while not ack_received:
+        packet = (str(seq) + segment).encode()
+        data_length = len(packet)
+        checksum = checksum_calculator(packet)
+        udp_header = struct.pack('!IIII', source_port, destination_port, data_length, int(checksum, 2))
+
+        packet_with_header = udp_header + packet
+
+        socket_object.sendto(packet_with_header, receiver_addr)
+
+        try:
+            message, address = recv_sock.recvfrom(1024)
+
+        except socket.timeout:
+            print("Timeout")
+        else:
+            full_segment = struct.unpack("!II", message)
+            checksum = full_segment[0]
+            ack_seq = full_segment[1]
+            if chr(ack_seq) == str(seq):
+            #if check_checksum_calculator(str(ack_seq).encode(), checksum) and chr(ack_seq) == str(seq):
+                ack_received = True
+
+    seq = 1 - seq
+
+socket_object.close()
