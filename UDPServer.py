f3e980ebf5e1dcf3ec2dafc8dd5b8a06170e38d6
vituriano <viturianooliveira@gmail.com>
2022-05-07 17:32:22 -0300

    fix the read function

--- UDPServer.py
+++ UDPServer.py
@@ -4,9 +4,6 @@ from checksum import checksum as checksum_calculator
 
 SEGMENT_SIZE = 16
 
-with open("teste.txt") as f:
-    content = f.read()
-
 source_port = 1111
 destination_port = 1112
 host = '127.0.0.1'
@@ -19,40 +16,40 @@ recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 recv_sock.bind(this_addr)
 recv_sock.settimeout(1)
 
-offset = 0
 seq = 0
 
-while offset < len(content):
-    if offset + SEGMENT_SIZE > len(content):
-        segment = content[offset:]
-    else:
-        segment = content[offset:offset + SEGMENT_SIZE]
-
-    offset += SEGMENT_SIZE - seq
-
-    ack_received = False
-
-    while not ack_received:
-        packet = (str(seq) + segment).encode()
-        data_length = len(packet)
-        checksum = checksum_calculator(packet)
-        udp_header = struct.pack('!IIIII', source_port, destination_port, data_length, int(checksum, 2), seq)
-
-        packet_with_header = udp_header + packet
-
-        socket_object.sendto(packet_with_header, receiver_addr)
-
-        try:
-            message, address = recv_sock.recvfrom(1024)
-
-        except socket.timeout:
-            print("Timeout")
+while True:
+    offset = 0
+    content = input()
+    while offset < len(content):
+        if offset + SEGMENT_SIZE > len(content):
+            segment = content[offset:]
         else:
-            checksum = message[:16]
-            ack_seq = message[19]
-            if checksum_calculator(message[16:]) == checksum.decode() and chr(ack_seq) == str(seq):
-                ack_received = True
+            segment = content[offset:offset + SEGMENT_SIZE]
 
-    seq = 1 - seq
+        offset += SEGMENT_SIZE - seq
 
-socket_object.close()
+        ack_received = False
+
+        while not ack_received:
+            packet = (str(seq) + segment).encode()
+            data_length = len(packet)
+            checksum = checksum_calculator(packet)
+            udp_header = struct.pack('!IIIII', source_port, destination_port, data_length, int(checksum, 2), seq)
+
+            packet_with_header = udp_header + packet
+
+            socket_object.sendto(packet_with_header, receiver_addr)
+
+            try:
+                message, address = recv_sock.recvfrom(1024)
+
+            except socket.timeout:
+                print("Timeout")
+            else:
+                checksum = message[:16]
+                ack_seq = message[19]
+                if checksum_calculator(message[16:]) == checksum.decode() and chr(ack_seq) == str(seq):
+                    ack_received = True
+
+        seq = 1 - seq
