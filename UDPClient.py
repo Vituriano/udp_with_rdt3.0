986d71485b496a163d0c064d75ccce55e7533ac8
vituriano <viturianooliveira@gmail.com>
2022-05-07 16:37:16 -0300

    refactor the functions]

--- UDPClient.py
+++ UDPClient.py
@@ -1,59 +1,13 @@
 import socket
-import zlib
+from checksum import checksum as checksum_calculator
 import struct
-import binascii
-
-def checksum_calculator(data):
-
-    data = bin(int(binascii.hexlify(data),16))
-   
-    c1 = data[0:16] or '0'
-    c2 = data[16:32] or '0'
-    c3 = data[32:48] or '0'
-    c4 = data[48:64] or '0'
- 
-    Sum = bin(int(c1, 2)+int(c2, 2)+int(c3, 2)+int(c4, 2))[2:]
-
-    if(len(Sum) > 16):
-        x = len(Sum)-16
-        Sum = bin(int(Sum[0:x], 2)+int(Sum[x:], 2))[2:]
-    if(len(Sum) < 16):
-        Sum = '0'*(16-len(Sum))+Sum
- 
-    Checksum = ''
-    for i in Sum:
-        if(i == '1'):
-            Checksum += '0'
-        else:
-            Checksum += '1'
-    return Checksum
-
-
-def check_checksum_calculator(data, checksum):
-   
-    data = bin(int(binascii.hexlify(data),16))
-    checksum = bin(int(binascii.hexlify(bytes(checksum)),16))
-
-    c1 = data[0:16] or '0'
-    c2 = data[16:32] or '0'
-    c3 = data[32:48] or '0'
-    c4 = data[48:64] or '0'
- 
-    ReceiverSum = bin(int(c1, 2)+int(c2, 2)+int(checksum, 2) +
-                      int(c3, 2)+int(c4, 2)+int(checksum, 2))[2:]
- 
-    if(len(ReceiverSum) > 16):
-        x = len(ReceiverSum)-16
-        ReceiverSum = bin(int(ReceiverSum[0:x], 2)+int(ReceiverSum[x:], 2))[2:]
-    if(len(ReceiverSum) < 16):
-        ReceiverSum = '0'*(16-len(ReceiverSum))+ReceiverSum
- 
-    return int(ReceiverSum, 2) == 0
 
 source_port = 1112
 destination_port = 1111
-receiver_addr = ('127.0.0.1', destination_port)
-this_addr = ('127.0.0.1', source_port)
+host = '127.0.0.1'
+
+receiver_addr = (host, destination_port)
+this_addr = (host, source_port)
 
 send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -73,9 +27,7 @@ while True:
     seq = udp_header[4]
     content = data[1:]
 
-    is_data_corrupted = check_checksum_calculator(data, correct_checksum)
-
-    if not is_data_corrupted:
+    if checksum_calculator(data) != correct_checksum:
         value = "ACK" + str(seq)
         checksum = checksum_calculator(value.encode())
         send_socket.sendto((checksum + value).encode(), receiver_addr)
--- UDPServer.py
+++ UDPServer.py
@@ -1,66 +1,19 @@
-from audioop import add
 import socket
 import struct
-import binascii
+from checksum import checksum as checksum_calculator
 
 SEGMENT_SIZE = 16
-
-def checksum_calculator(data):
-
-    data = bin(int(binascii.hexlify(data),16))
-   
-    c1 = data[0:16] or '0'
-    c2 = data[16:32] or '0'
-    c3 = data[32:48] or '0'
-    c4 = data[48:64] or '0'
- 
-    Sum = bin(int(c1, 2)+int(c2, 2)+int(c3, 2)+int(c4, 2))[2:]
-
-    if(len(Sum) > 16):
-        x = len(Sum)-16
-        Sum = bin(int(Sum[0:x], 2)+int(Sum[x:], 2))[2:]
-    if(len(Sum) < 16):
-        Sum = '0'*(16-len(Sum))+Sum
- 
-    Checksum = ''
-    for i in Sum:
-        if(i == '1'):
-            Checksum += '0'
-        else:
-            Checksum += '1'
-    return Checksum
-
-
-def check_checksum_calculator(data, checksum):
-   
-    data = bin(int(binascii.hexlify(data),16))
-    checksum = bin(int(binascii.hexlify(bytes(checksum)),16))
-
-    c1 = data[0:16] or '0'
-    c2 = data[16:32] or '0'
-    c3 = data[32:48] or '0'
-    c4 = data[48:64] or '0'
- 
-    ReceiverSum = bin(int(c1, 2)+int(c2, 2)+int(checksum, 2) +
-                      int(c3, 2)+int(c4, 2)+int(checksum, 2))[2:]
-
-    if(len(ReceiverSum) > 16):
-        x = len(ReceiverSum)-16
-        ReceiverSum = bin(int(ReceiverSum[0:x], 2)+int(ReceiverSum[x:], 2))[2:]
- 
-    return int(ReceiverSum, 2) == 0
 
 with open("teste.txt") as f:
     content = f.read()
 
-
 source_port = 1111
 destination_port = 1112
-receiver_addr = ('127.0.0.1', destination_port)
-this_addr = ('127.0.0.1', source_port)
+host = '127.0.0.1'
+receiver_addr = (host, destination_port)
+this_addr = (host, source_port)
 
 socket_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 
 recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 recv_sock.bind(this_addr)
