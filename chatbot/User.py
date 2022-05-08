85b4c19e0173f5a940425d7c91a8a15fa1787249
Ricardo Morato <ricardomoratodev@gmail.com>
2022-05-08 17:47:21 -0300

    feat: compute individual user's bill

--- chatbot/User.py
+++ chatbot/User.py
@@ -1,6 +1,8 @@
 from utils.menu import find_item
 
 class User:
+  bill = 0
+
   def __init__(self, id: int, name: str, socket: str, table_id: int) -> None:
       self.id = id
       self.name = name
@@ -11,24 +13,20 @@ class User:
   def add_order(self, order_id: int) -> None:
     order = find_item(order_id)
 
+    self.bill += order["price"]
+
     if order:
       self.orders.append(order)
 
-  def get_orders(self) -> int:
-    total = 0
-
+  def get_orders(self) -> None:
     print(f"| {self.name} |\n")
 
     for order in self.orders:
-      total += order['price']
-
       print(f"{order['name']} -> {order['price']}")
       print("--------------------------------------------------")
 
-    print(f"\nTotal: R$ {format(total, '.2f')}")
+    print(f"\nTotal: R$ {format(self.bill, '.2f')}")
     print("--------------------------------------------------\n")
-
-    return total
 
 if __name__ == "__main__":
   user = User(0, "Teste", "128.65.27.104:5000", 5)
